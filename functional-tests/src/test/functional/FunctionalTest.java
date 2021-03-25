e03939dacd2dee9b4bd9010e8e9207d44c140b54
Nohan75 <nohanmarielouise@gmail.com>
2021-03-25 15:03:13 +0100

    Test de la moitiée des critères de la fiche Meetup

--- functional-tests/src/test/functional/FunctionalTest.java
+++ functional-tests/src/test/functional/FunctionalTest.java
@@ -32,74 +32,101 @@ public class FunctionalTest {
     }
 
     // Test de la Story #1-homepage (https://trello.com/c/WKTneu9o/1-homepage)
-	@Test
-    public void testHomepage() throws Exception {
-        // get page title
-        driver.get("https://www.meetup.com/fr-FR/");
-        assertEquals(driver.getTitle(), "Partagez vos passions | Meetup");
+	// @Test
+    // public void testHomepage() throws Exception {
+    //     // get page title
+    //     driver.get("https://www.meetup.com/fr-FR/");
+    //     assertEquals(driver.getTitle(), "Partagez vos passions | Meetup");
         
-        // get h1 title
-        WebElement h1 = driver.findElement(By.cssSelector(".exploreHome-hero-mainTitle"));
-        String h1Content = h1.getText();
-        assertEquals(h1Content, "Le monde vous tend les bras");
+    //     // get h1 title
+    //     WebElement h1 = driver.findElement(By.cssSelector(".exploreHome-hero-mainTitle"));
+    //     String h1Content = h1.getText();
+    //     assertEquals(h1Content, "Le monde vous tend les bras");
 
-        // get h1 subtitle
-        WebElement subTitle = driver.findElement(By.cssSelector(".exploreHome-hero-subTitle"));
-        String subTitleContent = subTitle.getText();
-        assertEquals(subTitleContent, "Rejoignez un groupe local pour rencontrer du monde, tester une nouvelle activité ou partager vos passions.");
+    //     // get h1 subtitle
+    //     WebElement subTitle = driver.findElement(By.cssSelector(".exploreHome-hero-subTitle"));
+    //     String subTitleContent = subTitle.getText();
+    //     assertEquals(subTitleContent, "Rejoignez un groupe local pour rencontrer du monde, tester une nouvelle activité ou partager vos passions.");
 
-        // get meta description 
-        WebElement metaDescription = driver.findElement(By.cssSelector("meta[name=description]"));
-        String metaDescriptionContent = metaDescription.getAttribute("content");
-        assertTrue(metaDescriptionContent.contains("Partagez vos passions et faites bouger votre ville"));
+    //     // get meta description 
+    //     WebElement metaDescription = driver.findElement(By.cssSelector("meta[name=description]"));
+    //     String metaDescriptionContent = metaDescription.getAttribute("content");
+    //     assertTrue(metaDescriptionContent.contains("Partagez vos passions et faites bouger votre ville"));
 
-        // get button 
-        WebElement buttonDescription = driver.findElement(By.cssSelector("#exploreCTA a"));
-        String buttonContent = buttonDescription.getAttribute("href");
-        String buttonDescriptionContent = buttonDescription.getText();
-        assertTrue(buttonContent.contains("https://www.meetup.com/fr-FR/register/"));
-        assertEquals(buttonDescriptionContent, "Rejoindre Meetup");
+    //     // get button 
+    //     WebElement buttonDescription = driver.findElement(By.cssSelector("#exploreCTA a"));
+    //     String buttonContent = buttonDescription.getAttribute("href");
+    //     String buttonDescriptionContent = buttonDescription.getText();
+    //     assertTrue(buttonContent.contains("https://www.meetup.com/fr-FR/register/"));
+    //     assertEquals(buttonDescriptionContent, "Rejoindre Meetup");
 
-		// TODO
-		// To Be Completed By Coders From Coding Factory
-    }
+	// 	// TODO
+	// 	// To Be Completed By Coders From Coding Factory
+    // }
 
     // Test de la Story #2-recherche (https://www.meetup.com/fr-FR/find/outdoors-adventure/)
-    @Test
-    public void testSearchPage() throws Exception {
-        // get page title
-        driver.get("https://www.meetup.com/fr-FR/find/outdoors-adventure/");
-        assertTrue(driver.getTitle().contains("Nature et aventure"));
+    // @Test
+    // public void testSearchPage() throws Exception {
+    //     // get page title
+    //     driver.get("https://www.meetup.com/fr-FR/find/outdoors-adventure/");
+    //     assertTrue(driver.getTitle().contains("Nature et aventure"));
         
-        // get h1 title
-        WebElement h1 = driver.findElement(By.cssSelector(".text--display1"));
-        String h1Content = h1.getText();
-        assertTrue(h1Content.contains("Nature et aventure"));
+    //     // get h1 title
+    //     WebElement h1 = driver.findElement(By.cssSelector(".text--display1"));
+    //     String h1Content = h1.getText();
+    //     assertTrue(h1Content.contains("Nature et aventure"));
 
-        // get navbar
-        assertTrue(driver.findElements(By.cssSelector("#findNavBar")).size() > 0);
+    //     // get navbar
+    //     assertTrue(driver.findElements(By.cssSelector("#findNavBar")).size() > 0);
         
-        // get input search
-        //assertTrue(driver.findElements(By.cssSelector("#mainKeywords")).size() > 0);
+    //     // get input search
+    //     //assertTrue(driver.findElements(By.cssSelector("#mainKeywords")).size() > 0);
 
-        // get choix par défaut : pertinence
-        assertTrue(driver.findElement(By.cssSelector("a.selected[data-value=default]")).getAttribute("textContent").toLowerCase().contains("pertinence"));
+    //     // get choix par défaut : pertinence
+    //     assertTrue(driver.findElement(By.cssSelector("a.selected[data-value=default]")).getAttribute("textContent").toLowerCase().contains("pertinence"));
 
-        // get 4 choix de tri possible
-        List<WebElement> fourChoice = driver.findElements(By.cssSelector("#simple-find-order ul li"));
-        for(WebElement e : fourChoice) {
-            String attribu = e.getAttribute("textContent");
-            if (!e.getAttribute("class").contains("display-none")){
-                //System.out.println(e.getAttribute("textContent"));
-                assertTrue(attribu.contains("pertinence") || attribu.contains("plus récents") || 
-                           attribu.contains("nombre de membres") || attribu.contains("proximité"));
-            }
-        }
-    }
+    //     // get 4 choix de tri possible
+    //     List<WebElement> fourChoice = driver.findElements(By.cssSelector("#simple-find-order ul li"));
+    //     for(WebElement e : fourChoice) {
+    //         String attribu = e.getAttribute("textContent");
+    //         if (!e.getAttribute("class").contains("display-none")){
+    //             //System.out.println(e.getAttribute("textContent"));
+    //             assertTrue(attribu.contains("pertinence") || attribu.contains("plus récents") || 
+    //                        attribu.contains("nombre de membres") || attribu.contains("proximité"));
+    //         }
+    //     }
+    // }
 
     @Test
     public void testFicheMeetup() throws Exception {
         driver.get("https://www.meetup.com/fr-FR/promenades-et-randonnees/?_locale=fr-FR");
+
+        WebElement joinButton = driver.findElement(By.cssSelector("#actionButtonLink"));
+        joinButton.click();
+
+        WebElement logFacebook = driver.findElement(By.cssSelector(".signUpModal-facebook"));
+        String fbRedir = logFacebook.getAttribute("href");
+        assertTrue(fbRedir.contains("https://www.facebook.com/")); // Vérif du lien de connexion via Facebook
+
+        WebElement logGoogle = driver.findElement(By.cssSelector(".signUpModal-google"));
+        String ggRedir = logGoogle.getAttribute("href");
+        assertTrue(ggRedir.contains("https://accounts.google.com/o/oauth2/"));
+
+        WebElement logMeetup = driver.findElement(By.cssSelector("a.link.display--inlineBlock"));
+        String meetRedir = logMeetup.getAttribute("href");
+        assertTrue(meetRedir.contains("https://www.meetup.com/fr-FR/login/"));
+
+        WebElement signup = driver.findElement(By.cssSelector(".signUpModal-email"));
+        String signupRedir = signup.getAttribute("href");
+        assertTrue(signupRedir.contains("/register/?method=email"));
+
+        WebElement closeButton = driver.findElement(By.cssSelector(".modal-closeButton"));
+        closeButton.click();
+
+        WebElement contactButton = driver.findElement(By.cssSelector(".orgInfo-message"));
+        contactButton.click();
+        String currentUrl = driver.getCurrentUrl();
+        assertTrue(currentUrl.contains("https://secure.meetup.com/fr-FR/login/"));
 
     }
 
