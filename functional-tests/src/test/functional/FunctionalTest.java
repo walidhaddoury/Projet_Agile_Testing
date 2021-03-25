2e0efae5d9bef577953e3d14fdb4c98c663f4f65
Billy Christopher <billy_christopher@hotmail.com>
2021-03-25 15:29:16 +0100

    rassemblement des codes

--- functional-tests/src/test/functional/FunctionalTest.java
+++ functional-tests/src/test/functional/FunctionalTest.java
@@ -12,6 +12,8 @@ import static org.junit.Assert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
@@ -59,9 +61,6 @@ public class FunctionalTest {
         String buttonDescriptionContent = buttonDescription.getText();
         assertTrue(buttonContent.contains("https://www.meetup.com/fr-FR/register/"));
         assertEquals(buttonDescriptionContent, "Rejoindre Meetup");
-
-		// TODO
-		// To Be Completed By Coders From Coding Factory
     }
 
     // Test de la Story #2-recherche (https://www.meetup.com/fr-FR/find/outdoors-adventure/)
@@ -101,6 +100,75 @@ public class FunctionalTest {
     public void testFicheMeetup() throws Exception {
         driver.get("https://www.meetup.com/fr-FR/promenades-et-randonnees/?_locale=fr-FR");
 
+        // Controle si le titre existe et est cliquable
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement h1 = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("h1.groupHomeHeader-groupName")));
+        assertTrue(h1 instanceof WebElement);
+        // Controle si le lieu existe
+        assertTrue(driver.findElements(By.cssSelector("ul.organizer-city a span")).size() > 0);
+        // Controle si le total de membre existe
+        assertTrue(driver.findElements(By.cssSelector("a.groupHomeHeaderInfo-memberLink span")).size() > 0);
+        // Controle si l'organisateur existe
+        assertTrue(driver.findElements(By.cssSelector("a.orgInfo-name-superGroup span + span")).size() > 0);
+        // Controle si le bouton rejoindre le groupe existe
+        assertTrue(driver.findElements(By.cssSelector("a#actionButtonLink")).size() > 0);
+        // Controle si la photo de présentation existe
+        assertTrue(driver.findElements(By.cssSelector("div.groupHomeHeader-banner")).size() > 0);
+
+        List <WebElement> bandereauOnglet = driver.findElements(By.cssSelector(".groupAnchorBar div div div div div div ul li"));
+        for(WebElement menus : bandereauOnglet){
+            assertTrue(menus.getText().contains("À propos") || menus.getText().contains("Événements") || 
+                       menus.getText().contains("Membres") || menus.getText().contains("Photos") || 
+                       menus.getText().contains("Discussions") || menus.getText().contains("Plus"));
+        }
+
+        WebElement joinButton = driver.findElement(By.cssSelector("#actionButtonLink"));
+        joinButton.click();
+
+        WebElement logFacebook = driver.findElement(By.cssSelector(".signUpModal-facebook"));
+        String fbRedir = logFacebook.getAttribute("href");
+        assertTrue(fbRedir.contains("https://www.facebook.com/")); // Vérif du lien de connexion via Facebook
+
+        WebElement logGoogle = driver.findElement(By.cssSelector(".signUpModal-google"));
+        String ggRedir = logGoogle.getAttribute("href");
+        assertTrue(ggRedir.contains("https://accounts.google.com/o/oauth2/"));
+
+        WebElement logMeetup = driver.findElement(By.cssSelector("a.link.display--inlineBlock"));
+        String meetRedir = logMeetup.getAttribute("href");
+        assertTrue(meetRedir.contains("https://www.meetup.com/fr-FR/login/"));
+
+        WebElement signup = driver.findElement(By.cssSelector(".signUpModal-email"));
+        String signupRedir = signup.getAttribute("href");
+        assertTrue(signupRedir.contains("/register/?method=email"));
+
+        WebElement closeButton = driver.findElement(By.cssSelector(".modal-closeButton"));
+        closeButton.click();
+
+        WebElement contactButton = driver.findElement(By.cssSelector(".orgInfo-message"));
+        contactButton.click();
+        String currentUrl = driver.getCurrentUrl();
+        assertTrue(currentUrl.contains("https://secure.meetup.com/fr-FR/login/"));
+    }
+
+    @Test
+    public void testJobs() throws Exception{
+        driver.get("https://www.meetup.com/fr-FR/careers/");
+
+        String topPunchline = driver.findElement(By.cssSelector(".brandHero-slogan")).getText();
+        assertEquals(topPunchline, "Join our team, find your people");
+
+        String opportunitiesBtn = driver.findElement(By.cssSelector("._brandHero-module_brandHeroButton__37glx")).getText();
+        assertEquals(opportunitiesBtn, "Explore Opportunities");
+
+        List <WebElement> postOffers = driver.findElements(By.cssSelector("._ourTeams-module_ourTeams-list__1p-ci ul li"));
+        WebElement linkOpportunities = driver.findElement(By.cssSelector("._stayInTouch-module_StayInTouch__3nOZP div span a"));
+        assertEquals(linkOpportunities.getAttribute("href"), "https://www.meetup.com/careers/all-opportunities");
+        assertTrue(postOffers.size() == 9);
+
+        String perksPunchline = driver.findElement(By.cssSelector(".perksAndBenefits-title")).getText();
+        List <WebElement> perksImgs = driver.findElements(By.cssSelector("._perksAndBenefits-module_perksAndBenefits__3LekN ul li"));
+        assertTrue(perksImgs.size() == 6); 
+        assertEquals(perksPunchline, "Perks and benefits");
     }
 
     @After
